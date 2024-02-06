c636075e81d532391d62b4e21d3c5912dbb92669
Veronika Timakova <tymnik@ukr.net>
2024-02-07 00:52:33 +0200

    ProgressBar component and styles

--- src/components/Banner/Widget/ProgressBarRange/ProgressBarRange.jsx
+++ src/components/Banner/Widget/ProgressBarRange/ProgressBarRange.jsx
@@ -1,38 +1,48 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './ProgressBarRange.module.css';
 
 const ProgressBarRange = () => {
-  const rangeRef = useRef(null);
+  const sliderRef = useRef(null);
+  const [background, setBackground] = useState('');
 
-  const handleRangeClick = value => {
-    rangeRef.current.value = value;
+  const handleInput = event => {
+    const { value } = event.target;
+    const percent = (value / event.target.max) * 100;
+    setBackground(
+      `linear-gradient(to right, #DC4A19 , #FCC539 ${percent}%, var(--progress-range-bg)${percent}%)`
+    );
   };
 
   return (
-    <div className={styles.progressBarRange}>
-      <input
-        ref={rangeRef}
-        type="range"
-        className={styles.barToggle}
-        min="1"
-        max="10"
-        step="1"
-        aria-label="A range field for voting"
-      />
-      <div className={styles.barScale}>
-        <p>Note</p>
-        <ul>
-          <li onClick={() => handleRangeClick(1)}>1</li>
-          <li onClick={() => handleRangeClick(2)}>2</li>
-          <li onClick={() => handleRangeClick(3)}>3</li>
-          <li onClick={() => handleRangeClick(4)}>4</li>
-          <li onClick={() => handleRangeClick(5)}>5</li>
-          <li onClick={() => handleRangeClick(6)}>6</li>
-          <li onClick={() => handleRangeClick(7)}>7</li>
-          <li onClick={() => handleRangeClick(8)}>8</li>
-          <li onClick={() => handleRangeClick(9)}>9</li>
-          <li onClick={() => handleRangeClick(10)}>10</li>
-        </ul>
+    <div>
+      <div className={styles.fieldGroup}>
+        <label htmlFor="range" className={styles.rangeLabel}>
+          <p className={styles.barScaleTitle}>Note</p>
+          <ul className={styles.barScale}>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+          </ul>
+        </label>
+        <input
+          ref={sliderRef}
+          type="range"
+          name="range"
+          id="range"
+          className={styles.rangeField}
+          min={1}
+          max={10}
+          onInput={handleInput}
+          style={{ background }}
+          aria-label="A range field for voting"
+        />
       </div>
     </div>
   );
